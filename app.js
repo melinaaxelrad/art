9d5b02f94841bff9864fb2ce3c11c8093391a84e
melinaaxelrad <melinaaxelrad@gmail.com>
2025-12-25 18:03:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -499,9 +499,32 @@ async function renderHome() {
                     </div>
                     <button type="submit">Send Message</button>
                 </form>
-            </div>
-        </section>
-    `;
+                      class="contact-form"
+                      action="https://formspree.io/f/abcdwxyz"
+                      method="POST"
+                        >
+                        <input type="hidden" name="_subject" value="New message from art website">
+
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" id="name" name="name" required>
+                    </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" id="email" name="email" required>
+                    </div>
+
+                        <div class="form-group">
+                            <label for="message">Message</label>
+                            <textarea id="message" name="message" rows="5" required></textarea>
+                    </div>
+
+                        <button type="submit">Send Message</button>
+                        </form>
+                     </div>
+                    </section>
+                    `;
     
     renderGallery(artworks);
     loadAnnouncements();
